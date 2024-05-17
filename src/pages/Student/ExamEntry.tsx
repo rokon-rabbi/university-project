3c8c61f902c3948220bd28cb0dfb475001ff1f43
rokon <rokonrabbi21@gmail.com>
2024-05-17 10:00:36 +0600

    exam entry page frontend done

--- src/pages/Student/ExamEntry.tsx
+++ src/pages/Student/ExamEntry.tsx
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import DefaultLayout from '../../layout/DefaultLayout';
 import './examEntry.css'
 import logo from '../../assets/nstu.png'
 const ExamEntry = () => {
+    const [user, setUser] = useState<any>(null); // Initialize user state
+
+    useEffect(() => {
+        const storedUser = localStorage.getItem('user');
+        if (storedUser) {
+            setUser(JSON.parse(storedUser)); // Set user state from localStorage
+        }
+    }, []);
+
     return (
         <DefaultLayout>
 
             <>
-                <div className="container mx-auto p-4">
+                <div className="container mx-auto md:p-4">
                     <div className="form-header p-4 md:flex md:flex-col md:items-center md:justify-center">
                         <div className="flex justify-between items-center mb-4">
                             <img
-                                src={logo}
+                                src={user && user.user_image}
                                 alt="photo"
-                                className="object-cover object-center w-20 h-20 md:w-22 md:h-32 mr-4"
+                                className="object-cover object-center w-20 h-20 md:w-22 md:h-28 mr-4"
                             />
                             <img
                                 src={logo}
@@ -39,25 +48,37 @@ const ExamEntry = () => {
 
                     <div className="mb-4">
                         <div className="md:flex md:items-center">
-                            <label htmlFor="name" className="mr-2">Name:</label>
+                            <label htmlFor="name" className="md:mr-14">Name:</label>
                             <input type="text" name="name" className="input-field" placeholder="Name" />
                         </div>
                         <div className="md:flex md:items-center">
-                            <label htmlFor="fname" className="mr-2">Father's Name:</label>
+                            <label htmlFor="fname" className="md:mr-2">Father's Name:</label>
                             <input type="text" name='fname' className="input-field" placeholder="Father's Name" />
                         </div>
 
                         <div className="md:flex md:items-center">
-                            <label htmlFor="mname" className="mr-2">Mother's Name:</label>
+                            <label htmlFor="mname" className="md:mr-2">Mother's Name:</label>
                             <input type="text" name='mname' className="input-field" placeholder="Mothers's Name" />
                         </div>
-                        <input type="text" className="input-field" placeholder="Department" />
+                        <div className="md:flex md:items-center">
+                            <label htmlFor="department" className="md:mr-6">Department :</label>
+                            <input type="text" name='department' className="input-field" placeholder="Department" />
+                        </div>
+
+
                         <div className="flex items-center">
                             <label>Roll NO.: </label>
                             <div className="border-bottom flex-grow ml-2">___________________________</div>
                         </div>
-                        <input type="text" className="input-field" placeholder="Name of the Hall" />
-                        <input type="text" className="input-field" placeholder="Date of commencement of Examination" />
+                        <div className="md:flex md:items-center">
+                            <label htmlFor="hall:" className="md:mr-6">Name of the Hall:</label>
+                            <input type="text" name='hall' className="input-field" placeholder="Name of the Hall" />
+                        </div>
+                        <div className="md:flex md:items-center">
+                            <label htmlFor="date" className="md:mr-6">Date of commencement of Examination:</label>
+                            <input type="text" name='date' className="input-field" placeholder="Date of commencement of Examination" />
+                        </div>
+
                     </div>
 
                     <table className="table-auto w-full mb-4">
@@ -88,39 +109,63 @@ const ExamEntry = () => {
                         <label>Percentage of attendance: ______________</label>
                     </div>
 
-                    <div className="signature-block mb-8">
+                    <div className="signature-block p-10 md:mt-18 md:mb-6">
                         <div>Chairman</div>
                         <div>Provost</div>
                     </div>
 
                     <div className="admit-card p-4">
                         <div className="flex justify-between items-center mb-4">
-                            <div className="stamp-photo"></div>
+                            <img
+                                src={logo}
+                                alt="nstu logo"
+                                className="object-cover object-center w-12 h-12 md:w-14 md:h-20"
+                            />
                             <div className="text-center">
-                                <h2>Noakhali Science and Technology University</h2>
+                                <h6 className='text-2xl'>Noakhali Science and Technology University</h6>
                                 <div className="text-lg">Admit Card</div>
-                                <div>Year: ________ Session: ________ Term: ________ Final Examination-202_</div>
+                                <div className="flex md:text-left flex-col md:flex-row">
+                                    <span className="mr-4 mb-2 md:mb-0">Year: ________</span>
+                                    <span className=" mb-2 md:mb-0">Session: ________</span>
+                                    <span className="mb-2 md:mb-0">Term: ________</span>
+                                    <span className="md:ml-auto">Final Examination-202_</span>
+                                </div>
+
                             </div>
-                            <div></div>
+                            <div>
+                                <img
+                                    src={user && user.user_image}
+                                    alt="photo"
+                                    className="object-cover object-center w-20 h-20 md:w-22 md:h-28 mr-4"
+                                />
+                            </div>
                         </div>
-                        <div className="flex justify-between mb-4">
-                            <input type="text" className="input-field" placeholder="Name" />
-                            <input type="text" className="input-field" placeholder="Department" />
+                        <div className=" mb-4">
+                            <div className="md:flex md:items-center">
+                                <label htmlFor="name" className="md:mr-14">Name:</label>
+                                <input type="text" name="name" className="input-field" placeholder="Name" />
+                            </div>
+                            <div className="md:flex md:items-center">
+                                <label htmlFor="department" className="md:mr-6">Department:</label>
+                                <input type="text" name='department' className="input-field" placeholder="Department" />
+                            </div>
                         </div>
                         <div className="flex items-center">
                             <label>Roll NO.: </label>
                             <div className="border-bottom flex-grow ml-2">___________________________</div>
                         </div>
-                        <input type="text" className="input-field" placeholder="Name of the Hall" />
-                        <input type="text" className="input-field" placeholder="Date of commencement of Examination" />
+                        <div className="md:flex md:items-center">
+                            <label htmlFor="hall:" className="md:mr-6">Name of the Hall:</label>
+                            <input type="text" name='hall' className="input-field" placeholder="Name of the Hall" />
+                        </div>
+                        <div className="md:flex md:items-center">
+                            <label htmlFor="date" className="md:mr-6">Date of commencement of Examination:</label>
+                            <input type="text" name='date' className="input-field" placeholder="Date of commencement of Examination" />
+                        </div>
                     </div>
 
-                    <div className="mb-4">
-                        <label className="signature-field inline-block w-1/2 text-center">Controller of Examinations</label>
-                        <div className="inline-block w-1/2 text-center">
-                            <span>Stamp Size Photograph:</span>
-                            <div className="border-2 border-dashed h-20 w-16 inline-block ml-2"></div>
-                        </div>
+                    <div className="mt-14 flex justify-end">
+                        <label className="signature-field w-1/2 text-center float-right">Controller of Examinations</label>
                     </div>
 
                 </div>
